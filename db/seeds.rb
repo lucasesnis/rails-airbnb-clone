3c989a869997b0615471b85506aad95c428a6835
Lucas Esnis <esnis.lucas@gmail.com>
2021-02-15 15:50:25 +0100

    first seed for service

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Service.destroy_all
+Service.create!(title: "Mécanicien", description: "Je répare vos bateaux")
