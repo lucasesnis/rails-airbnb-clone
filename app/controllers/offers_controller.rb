c686046eaf6198490db487f75fb8f520ee2685d1
Charles Fousse <cefsdp@gmail.com>
2021-02-15 16:08:04 +0100

    function for new and create added on Offers#Controller

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,2 +1,22 @@
 class OffersController < ApplicationController
+  def new
+    @offer = Offer.new
+  end
+
+  def create
+    @offer = Offer.new(params[:Offer])
+    if @offer.save
+      flash[:success] = "Offer successfully created"
+      redirect_to @offer
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
+  end
+
+  private
+
+  def offer_params
+    params.require(:offer).permit(:title, :description, :price, :user)
+  end
 end
