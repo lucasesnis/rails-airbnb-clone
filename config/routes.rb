b9877bfc04b128ac904b3316abb7c91cd0f48e1a
Charles Fousse <cefsdp@gmail.com>
2021-02-16 17:04:02 +0100

    Author updated and routes updates

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :show]
   resources :services, only: [:index, :show]
+  resources :transactions, only: [:index]
   resources :offers do
-    resources :transactions, except: [:destroy]
+    resources :transactions, except: [:destroy, :index]
   end
 
 end
