3d4c00f61049820aaaaa5a04be8f5ff644c258dd
Charles Fousse <cefsdp@gmail.com>
2021-02-16 12:08:08 +0100

    Added Policies file and add Show/Index for Offers/Services

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,10 +1,11 @@
 class ServicesController < ApplicationController
   def index
-    @services = Service.all
+    @services = policy_scope(Service)
   end
 
   def show
     @service = Service.find(params[:id])
-    @offers = Offer.where(service_id: params[:id])
+    authorize @service
+    # @offers = Offer.where(service_id: params[:id])
   end
 end
---
+++ app/policies/offer_policy.rb
@@ -0,0 +1,19 @@
+class OfferPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    return true
+  end
+
+  def new?
+    create?
+  end
+
+  def create?
+    return true
+  end
+end
