032ce710f075616aac04458c51da1595d791a455
Charles Fousse <cefsdp@gmail.com>
2021-02-19 13:13:01 +0100

    Merging

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -12,15 +12,6 @@ const flatpickrInit = () => {
 	});
   }
 
-  const calendar = document.querySelector('.datepickerbooking');
-  if (calendar) {
-	flatpickr(".datepickerbooking", {
-	    minDate: "today",
-		inline: true,
-	    dateFormat: "d-m-Y",
-	});
-  }
-
 }
 
 export { flatpickrInit };
